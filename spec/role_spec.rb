dcee06506592cbb73893c7fa3cefacc925bbda84
MorganFujimaka <olegkariuk@gmail.com>
2014-07-10 12:06:27 +0300

    Update role_spec.rb

--- spec/role_spec.rb
+++ spec/role_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe Role do
   
   describe "#initialize" do
-  	it "sets name, sex and age for the role" do
-  	  role = Role.new("Hulk", "male", 30..40)
-  	  expect(role.name).to eq("Hulk")
-  	  expect(role.sex).to eq("male")
-  	  expect(role.age).to eq(30..40)
-  	end
+    it "sets name, sex and age for the role" do
+    role = Role.new("Hulk", "male", 30..40)
+    expect(role.name).to eq("Hulk")
+    expect(role.sex).to eq("male")
+    expect(role.age).to eq(30..40)
+  end
   end
 	
 end
